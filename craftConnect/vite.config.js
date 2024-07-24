bb125f6df9a91f53b721ec681ad4490531beaac3
YksRocks <yk66478@gmail.com>
2024-07-24 22:51:57 +0530

    chore: Update vite.config.js

--- craftConnect/vite.config.js
+++ craftConnect/vite.config.js
@@ -4,14 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["react", "react-dom", "react-router-dom"], // Externalize React and ReactDOM
-    },
-  },
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  },
+ 
 });
