5e56fd13c8f246967278c476049657ee03983c3e
YksRocks <yk66478@gmail.com>
2024-07-25 01:31:33 +0530

    refactor: Update user routes to use authentication middleware

--- backend_craftConnect/routes/users.js
+++ backend_craftConnect/routes/users.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import passport from "passport";
 import * as userController from "../controllers/user.js";
-
+import { isAuthenticated } from "../middlewares/auth.js";
 const router = Router();
 
 // router.post("/register", userController.register);
 // router.post("/login", passport.authenticate("local"), userController.login);
 router.get("/profile/:id", userController.getProfile);
+router.put("/update", isAuthenticated, userController.updateProfile);
 
 export default router;
