8f9db5f1b2ae76d809db43456907275f202c7472
YksRocks <rs3209438@gmail.com>
2024-07-26 19:59:25 +0530

    feat: Add useEffect hook to EditUserForm.jsx

--- frontend_craftConnect/src/components/EditUserForm/EditUserForm.jsx
+++ frontend_craftConnect/src/components/EditUserForm/EditUserForm.jsx
@@ -1,5 +1,5 @@
 // components/EditUserForm/EditUserForm.jsx
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "../../api/axios";
 
 const EditUserForm = ({ currentUser }) => {
