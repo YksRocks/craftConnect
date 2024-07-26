55af0713e9399806a73a0b9061c32b8eefdee86b
YksRocks <rs3209438@gmail.com>
2024-07-26 20:03:48 +0530

    refactor: Update EditUserForm to include isLoggedIn prop

--- frontend_craftConnect/src/components/EditUserForm/EditUserForm.jsx
+++ frontend_craftConnect/src/components/EditUserForm/EditUserForm.jsx
@@ -2,7 +2,7 @@
 import { useState, useEffect } from "react";
 import axios from "../../api/axios";
 
-const EditUserForm = ({ currentUser }) => {
+const EditUserForm = ({ currentUser, isLoggedIn }) => {
   const [formData, setFormData] = useState({
     username: "",
     email: "",
