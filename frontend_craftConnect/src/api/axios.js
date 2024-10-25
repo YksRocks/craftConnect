50a0df8f8ee414f15ac42beb1ef0e1f4e4e8719d
Yash Kumar Singh <73733249+YksRocks@users.noreply.github.com>
2024-10-26 02:41:11 +0530

    Update axios.js

--- frontend_craftConnect/src/api/axios.js
+++ frontend_craftConnect/src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://craftconnect-c5w4.onrender.com",
+  baseURL: "https://craftconnect-c5w4.onrender.com/api",
   withCredentials: true, // This will send the cookies along with requests
 });
 
